7494683cacb2b3dcd6026b48b665ca80a97cf4bb
tales-zup <tales.araujo@zup.com.br>
2021-08-16 21:50:50 -0300

    enum categoria e construtor em produto

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,21 +1,18 @@
 package br.com.alura.loja.testes;
 
 import br.com.alura.loja.dao.ProdutoDAO;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.math.BigDecimal;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
-        Produto produto = new Produto();
-        produto.setNome("Xiaomi");
-        produto.setDescricao("Topzera");
-        produto.setPreco(new BigDecimal(800));
+        Produto produto = new Produto(
+                "Xiaomi", "Topzera", new BigDecimal(800), Categoria.CELULARES);
 
         EntityManager em = JPAUtil.getEntityManager();
         ProdutoDAO dao = new ProdutoDAO(em);
